3b2d09ed33e22cc4e2391e0ecb40a3dac382cae2
Adrian Garmendia <dejima@gmail.com>
2020-06-09 11:25:31 -0500

    ch 1

---
+++ src/components/locations/LocationCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LocationCard = () => {
+	return (
+		<div className="card">
+			<div className="card-content">
+				<h3>
+					Name: <span className="card-locationname">Donelson</span>
+				</h3>
+				<p>Address: 123 Donelson way</p>
+			</div>
+		</div>
+	);
+};
+
+export default LocationCard;
---
+++ src/components/owners/OwnerCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const OwnerCard = () => {
+	return (
+		<div className="card">
+			<div className="card-content">
+				<h3>
+					Name: <span className="card-ownername">Dr John</span>
+				</h3>
+				<p>Address: 111 Corondolet Ave</p>
+			</div>
+		</div>
+	);
+};
+
+export default OwnerCard;
